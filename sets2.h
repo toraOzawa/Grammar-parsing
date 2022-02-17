92edaf584433830475029fe393abad4ff919d8a6
Todo25 <87049757+toraOzawa@users.noreply.github.com>
2022-02-17 10:33:15 -0500

    part 1 v2

---
+++ sets2.h
@@ -0,0 +1,131 @@
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include "tree.h"
+
+
+// ⟨Expression⟩ → ⟨Expression⟩ U ⟨Expression⟩
+// ⟨Expression⟩ → ⟨Expression⟩ ^ ⟨Expression⟩
+// ⟨Expression⟩ → ( ⟨Expression⟩ )
+// ⟨Expression⟩ → ⟨Set⟩
+// ⟨Set⟩ → {}
+// ⟨Set⟩ → { ⟨Elements⟩ }
+// ⟨Elements⟩ → ⟨Element⟩
+// ⟨Elements⟩ → ⟨Elements⟩ , ⟨Element⟩
+// ⟨Element⟩ → ⟨Number ⟩
+// ⟨Number ⟩ → ⟨Digit⟩
+// ⟨Number ⟩ → ⟨Number ⟩ ⟨Digit⟩
+// ⟨Digit⟩ → 0 | 1 | · · · | 9
+
+
+// ⟨Expr ⟩ → ⟨Atomic⟩ ⟨ExprTail⟩              (add to tree)
+// ⟨ExprTail⟩ → U ⟨Expr ⟩ | ^ ⟨Expr ⟩ | ϵ
+// ⟨Atomic⟩ → ( ⟨Expr ⟩ ) | ⟨Set⟩
+// ⟨Set⟩ → { ⟨SetTail⟩                        (add to tree)
+// ⟨SetTail⟩ → } | ⟨Elements⟩ }
+// ⟨Elements⟩ → ⟨Element⟩ ⟨ElementsTail⟩      (add to tree)
+// ⟨ElementsTail⟩ → , ⟨Elements⟩ | ϵ
+// ⟨Element⟩ → ⟨Number⟩                      (add to tree)
+// ⟨Number ⟩ → ⟨Digit⟩ ⟨NumberTail⟩           (add to tree)
+// ⟨NumberTail⟩ → ⟨Number ⟩ | ϵ
+// ⟨Digit⟩ → 0 | 1 | · · · | 9                 (add to tree)
+
+extern bool expression();
+
+extern bool expr_tail();
+
+extern bool atomic();
+
+extern bool set();
+
+extern bool set_tail();
+
+extern bool elements();
+
+extern bool element();
+
+extern bool elements_tail();
+
+
+extern bool number();
+
+extern bool number_tail();
+
+
+extern bool digit();
+
+extern bool is_digit();
+
+// checks if the current char is the char inputed
+extern bool lookahead(char c);
+
+// checks if the current char is the char inputed and consumes it if it is
+extern bool match(char c);
+
+// checks if the input string is at the end 
+extern bool at_end();
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+// ⟨Expression⟩ → ⟨Expression⟩ U ⟨Expression⟩
+// ⟨Expression⟩ → ⟨Expression⟩ ^ ⟨Expression⟩
+// ⟨Expression⟩ → ( ⟨Expression⟩ )
+// ⟨Expression⟩ → ⟨Set⟩
+// ⟨Set⟩ → {}
+// ⟨Set⟩ → { ⟨Elements⟩ }
+// ⟨Elements⟩ → ⟨Element⟩
+// ⟨Elements⟩ → ⟨Elements⟩ , ⟨Element⟩
+// ⟨Element⟩ → ⟨Number ⟩
+// ⟨Number ⟩ → ⟨Digit⟩
+// ⟨Number ⟩ → ⟨Number ⟩ ⟨Digit⟩
+// ⟨Digit⟩ → 0 | 1 | · · · | 9
+
+
+// ⟨Expr ⟩ → ⟨Atomic⟩ ⟨ExprTail⟩              (add to tree)
+// ⟨ExprTail⟩ → U ⟨Expr ⟩ | ^ ⟨Expr ⟩ | ϵ
+// ⟨Atomic⟩ → ( ⟨Expr ⟩ ) | ⟨Set⟩
+// ⟨Set⟩ → { ⟨SetTail⟩                        (add to tree)
+// ⟨SetTail⟩ → } | ⟨Elements⟩ }
+// ⟨Elements⟩ → ⟨Element⟩ ⟨ElementsTail⟩      (add to tree)
+// ⟨ElementsTail⟩ → , ⟨Elements⟩ | ϵ
+// ⟨Element⟩ → ⟨Number⟩                      (add to tree)
+// ⟨Number ⟩ → ⟨Digit⟩ ⟨NumberTail⟩           (add to tree)
+// ⟨NumberTail⟩ → ⟨Number ⟩ | ϵ
+// ⟨Digit⟩ → 0 | 1 | · · · | 9                 (add to tree)
+
+extern bool expression();
+
+extern bool expr_tail();
+
+extern bool atomic();
+
+extern bool set();
+
+extern bool set_tail();
+
+extern bool elements();
+
+extern bool element();
+
+extern bool elements_tail();
+
+
+extern bool number();
+
+extern bool number_tail();
+
+
+extern bool digit();
+
+extern bool is_digit();
+
+// checks if the current char is the char inputed
+extern bool lookahead(char c);
+
+// checks if the current char is the char inputed and consumes it if it is
+extern bool match(char c);
+
+// checks if the input string is at the end 
+extern bool at_end();
